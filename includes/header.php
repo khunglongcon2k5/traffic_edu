1dd57a9ad5e2f6facad717442aa73a8c4087f416
Nguyễn Hùng <nguyentanhung413@gmail.com>
2025-06-23 19:46:11 +0700

    update

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <div class="container-header">
         <nav>
             <ul class="nav-menu">
-                <li><a href="../index.php">Chọn Phần Thi</a></li>
+                <li><a href="#">Chọn Phần Thi</a></li>
                 <li><a href="thi-bang-lai-xe-a1-online.php"
                         <?php if ($current_page == 'thi-bang-lai-xe-a1-online.php') echo 'class="active"'; ?>>Thi Thử
                         A1</a></li>
@@ -17,9 +17,9 @@
                         Điểm Liệt A2</a></li>
                 <li>
                     <?php if (isset($_SESSION['user']['name'])): ?> <div class="user-info">
-                        <i class="fa-solid fa-user-tie"></i><?php echo htmlspecialchars($_SESSION['user']['name']); ?>
-                        <a href="../includes/logout.php" class="btn btn-logout">Đăng xuất</a>
-                    </div>
+                            <i class="fa-solid fa-user-tie"></i><?php echo htmlspecialchars($_SESSION['user']['name']); ?>
+                            <a href="../includes/logout.php" class="btn btn-logout">Đăng xuất</a>
+                        </div>
                     <?php endif; ?>
                 </li>
             </ul>
